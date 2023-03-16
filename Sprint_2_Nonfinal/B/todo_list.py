328e2e21d05d974e37df85e06c32e33b40ddaf0f
whodef <tatyana@selyuk.com>
2023-03-16 18:51:29 +0200

    fix: 'ToDo list' solution added

--- Sprint_2_Nonfinal/B/todo_list.py
+++ Sprint_2_Nonfinal/B/todo_list.py
@@ -40,7 +40,7 @@ LOCAL = False
 
 if LOCAL:
     class Node:
-        def __init__(self, value, next_item=None):
+        def init(self, value, next_item=None):
             self.value = value
             self.next_item = next_item
 
